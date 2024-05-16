a386f048705f68d85f09c924c9084c175da6017a
Prashant Shukla <prashantshukla1029@gmail.com>
2024-05-16 21:00:13 +0530

    practiced spring mvc

---
+++ Thymeleaf/thymeleafdemo/src/main/java/com/prash/thymeleafdemo/enitity/Student.java
@@ -0,0 +1,40 @@
+package com.prash.thymeleafdemo.enitity;
+
+public class Student {
+    private String firstName;
+    private String lastName;
+    private String country;
+
+    public Student () {
+
+    }
+
+    public Student(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
