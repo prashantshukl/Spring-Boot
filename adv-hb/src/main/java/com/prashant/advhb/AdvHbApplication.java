39fe39c4ed29cfea283b2fab44e7a5f20eb39b15
Prashant Shukla <prashantshukla1029@gmail.com>
2024-05-23 10:49:05 +0530

    commit

---
+++ adv-hb/src/main/java/com/prashant/advhb/AdvHbApplication.java
@@ -0,0 +1,62 @@
+package com.prashant.advhb;
+
+import com.prashant.advhb.dao.AppDAO;
+import com.prashant.advhb.entity.Course;
+import com.prashant.advhb.entity.Instructor;
+import com.prashant.advhb.entity.InstructorDetail;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class AdvHbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvHbApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(AppDAO dao) {
+		return runner -> {
+			//createInstructor(dao);
+			//findInstructor(dao);
+			addCourses(dao);
+		};
+
+	}
+
+	private void addCourses(AppDAO dao) {
+		Instructor instructor = new
+				Instructor("kunal", "kushwaha", "kunal@love2code.com");
+
+		InstructorDetail detail = new InstructorDetail("www.youtube.com/kk", "loves tech");
+
+		instructor.setInstructorDetail(detail);
+
+		Course dsa = new Course("DSA");
+		Course dev = new Course("Dev-Ops");
+		instructor.add(dsa);
+		instructor.add(dev);
+		System.out.println("Saving Instructor :" + instructor);
+		dao.save(instructor);
+	}
+
+	private void findInstructor(AppDAO dao) {
+		Instructor theInstructor = dao.findInstructorById(2);
+		System.out.println(theInstructor);
+	}
+
+	private void createInstructor(AppDAO dao) {
+		Instructor instructor = new
+				Instructor("kunal", "kushwaha", "kunal@love2code.com");
+
+		InstructorDetail detail = new InstructorDetail("www.youtube.com/kk", "loves tech");
+
+		instructor.setInstructorDetail(detail);
+
+		System.out.println("Saving Instructor :" + instructor);
+		dao.save(instructor);
+	}
+
+}
---
+++ adv-hb/src/main/java/com/prashant/advhb/entity/Course.java
@@ -0,0 +1,60 @@
+package com.prashant.advhb.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "course")
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "title")
+    private  String title;
+
+
+
+    @ManyToOne(cascade = {CascadeType.MERGE, CascadeType.DETACH, CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinColumn(name = "instructor_id")
+    private Instructor instructor;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Course() {}
+
+    public Course(String title) {
+        this.title = title;
+    }
+
+    public Instructor getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(Instructor instructor) {
+        this.instructor = instructor;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                '}';
+    }
+}
